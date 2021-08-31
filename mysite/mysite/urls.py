cf63c3e18ce823b221c12a07f86fb93ab169c0b9
vpprilipukhov <vpprilipukhov@gmail.com>
2021-08-31 16:17:32 +0300

    Главная страница переходит на bboard

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+from django.conf.urls import  url
 
 
 urlpatterns = [
 
+    url(r'^', include('bboard.urls')),
     path('admin/', admin.site.urls),
     path('bboard/',include('bboard.urls')),
 
 ]
+
+
